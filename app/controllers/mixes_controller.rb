b7bdf6fa6fb8e7f4d6790d1a03f0486bdfe428a9
Reuben <reuben.telushkin@gmail.com>
2015-08-26 11:14:04 -0400

    user cannot delete other users posts, users and mixes id attributed to each other

--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -1,5 +1,9 @@
 class MixesController < ApplicationController
 
+	def show
+		@mixes = Mix.find(params[:id])
+	end
+
 	def new
 		@mix = Mix.new
 	end
@@ -11,13 +15,23 @@ class MixesController < ApplicationController
   def create
   	@mix = Mix.create(mix_params) #TODO
   	@mix.user = current_user #depends on mix belongs_to :user in model
+  	@mix.save
   	redirect_to mixes_path
   end
 
+def edit
+	@mix = Mix.find(params[:id])
+end
+
 def destroy
 	@mixes = Mix.find(params[:id])
-	@mixes.destroy
-	redirect_to mixes_path
+	if current_user.blank? or @mixes.user != current_user
+		flash[:alert] = 'unauthorized deletion attempt'
+		redirect_to mixes_path
+	else
+		@mixes.destroy
+		redirect_to mixes_path
+	end
 end
 
   private
