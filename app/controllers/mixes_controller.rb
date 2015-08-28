4f0be8363a88ddc3de5bd555050cb47b6e6b104f
Jeremy Raines <jraines@gmail.com>
2015-08-28 14:03:11 -0400

    Change mixes to tracks in the UI; give track a user_id

--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -1,53 +1,6 @@
 class MixesController < ApplicationController
 
-	def index
-		@mixes = Mix.order(created_at: :desc)
-		@mix = Mix.new
-	end
+  def index
 
-	def show
-		@mixes = Mix.find(params[:id])
-	end
-
-	def new
-		@mix = Mix.new
-	end
-
-	def edit
-	@mix = Mix.find(params[:id])
-	end
-
-	def create
-		@mix = Mix.new(mix_params)
-		@mix.user = current_user
-		@mix.save
-		redirect_to mixes_path
-	end
-
-	def update
-		@mixes = Mix.find(params[:id])
-		if @mixes.update(mix_params)
-		redirect_to mixes_path
-		else
-		render 'edit'
-		end
-	end
-
-def destroy
-	@mixes = Mix.find(params[:id])
-	if current_user.blank? or @mixes.user != current_user
-		flash[:alert] = 'unauthorized deletion attempt'
-		redirect_to mixes_path
-	else
-		@mixes.destroy
-		redirect_to mixes_path
-	end
-end
-
-  private
-
-  def mix_params
-    params.require(:mix).permit(:text, :title)
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826153405) do
+ActiveRecord::Schema.define(version: 20150828175449) do
 
   create_table "mixes", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150826153405) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
